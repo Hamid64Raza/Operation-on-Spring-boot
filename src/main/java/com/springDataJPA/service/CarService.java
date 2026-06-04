6d5a36f49bdd1abbfe9410f8f1cba43ec64d3078
Hamid64Raza <hamidraza.raza@gamil.com>
2026-06-04 12:08:55 +0530

    Car CRUD ops done

--- src/main/java/com/springDataJPA/service/CarService.java
+++ src/main/java/com/springDataJPA/service/CarService.java
@@ -2,9 +2,17 @@ package com.springDataJPA.service;
 
 import com.springDataJPA.entity.Car;
 
+import java.util.List;
+
 public interface CarService {
 
     public  Car saveCar(Car car);
+    public List<Car> getCars();
+
+    Car updateCar(long id, Car car);
+
+    public  Car deleteCarById(Long id);
+
 
 
 }
